38214abe882fded831a3c790a0da35bfff4ef2a4
attilalb <attila.l.bognar@protonmail.com>
2023-08-10 16:34:26 +0200

    update copy and break form submission

--- app/page.jsx
+++ app/page.jsx
@@ -1,17 +1,23 @@
 import Feed from "@components/Feed";
-
+import Link from "next/link";
 const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
       <h1 className="head_text text-center">
-        Discover & Share <br className="max-md:hidden" />
-        <span className="orange_gradient text-center">AI-Powered Prompts</span>
+        Share your{" "}
+        <span className="text-green-500 text-center">little wins.</span>
       </h1>
-      <p className="desc text-center">
-        Promptshare is an open-source platform where you can create and share
-        your favorite AI prompts and discover what others shared.
+      <p className="desc mt-6 text-center w-1/2">
+        littlewins is an open-source platform to share whatever you are proud of
+        with the world.
       </p>
 
+      <Link
+        href="/create-prompt"
+        className="rounded-full border mt-6 border-green-500 bg-green-500 py-1.5 px-5 text-white transition-all hover:bg-white hover:text-green-500 text-center text-sm font-inter flex items-center justify-center"
+      >
+        Start Sharing
+      </Link>
       <Feed />
     </section>
   );
